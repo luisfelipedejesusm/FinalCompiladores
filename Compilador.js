14bfb970d36a3adbf7fd64ce07fd82b863c75eea
De Jesus Muñoz <s2832435@intl.bns>
2019-08-07 16:10:46 -0400

    Main Class Done

--- Compilador.js
+++ Compilador.js
@@ -1,12 +1,13 @@
 class Compilador{
     constructor(){
-        this.vars_r = null
+        this.vars_r = {}
+        this.vars_array = []
         this.exp = null
         this.vars = []
         this.vars_negative = []
         this.result = ""
         this.error_msg = null
-        this.specialChars = ["→", "↔", "¬", "∧", "∨", "⊕"]
+        this.specialChars = ["→", "↔", "∧", "∨", "⊕"]
     }
     with(code){
         this.exp = code.replace(/ /g, '')
@@ -31,17 +32,19 @@ class Compilador{
         return true
     }
     _continuousOperators(){
-        for(var k in (temp = [...this.exp])){
-            if(this.specialChars.includes(temp[k]) && this.specialChars.includes(temp[k-1])){
+        var temp = [...this.exp]
+        for(var k in temp){
+            if(this.specialChars.includes(temp[~~k]) && this.specialChars.includes(temp[~~k-1])){
                 return true
             }
         }
         return false
     }
     _continuousLetter(){
-        for(var k in (temp = [...this.exp])){
-            if(k == 0) continue;
-            if(temp[k].match(/[A-Za-z]/g) && temp[k-1].match(/[A-Za-z]/g)){
+        var temp = [...this.exp]
+        for(var k in temp){
+            if(~~k == 0) continue;
+            if(temp[~~k].match(/[A-Za-z]/g) && temp[~~k-1].match(/[A-Za-z]/g)){
                 return true
             }
         }
@@ -57,17 +60,28 @@ class Compilador{
         }
         return p_c != 0
     }
-    get(){}
+    get(){
+        if(this._validate()){
+            var res = this._printVars()._printArrayP(this._getIndex(this.exp))._calcFunction(this.exp)
+            return {
+                vars: res.vars_array,
+                values: res.vars_r
+            }
+        }else{
+            return this.error_msg
+        }
+    }
     _createTable(){}
     _getIndex(exp){
         var match = false, after_matches = 0, match_index_start = [], match_index_end = []
-        for(var k in (temp = [...exp])){
-            if(temp[k] == '('){
+        var temp = [...exp];
+        for(var k in temp){
+            if(temp[~~k] == '('){
                 if(match){
                     after_matches++
                 }else{
                     match = true
-                    match_index_start.push(k)
+                    match_index_start.push(~~k)
                 }
             }
             if(temp[k] == ')'){
@@ -75,7 +89,7 @@ class Compilador{
                     after_matches--
                 }else{
                     match = false
-                    match_index_end.push(k)
+                    match_index_end.push(~~k)
                 }
             }
         }
@@ -104,18 +118,19 @@ class Compilador{
     }
     _customPregMatch(string){
         var data = [], p = 0, start = 0, end = null, last = false, neg_f = false
-        for(var k in (temp = [...string])){
-            switch(temp[k]){
+        var temp = [...string]
+        for(var k in temp){
+            switch(temp[~~k]){
                 case '→':
                 case '↔':
                 case '∧':
                 case '∨':
                 case '⊕':
                     if(p == 0){
-                        end = k
+                        end = ~~k
                         data.push(string.substr(start, end - start))
-                        data.push(temp[k])
-                        start = key + 1
+                        data.push(temp[~~k])
+                        start = ~~k + 1
                     }
                     break
                 case '(':
@@ -124,17 +139,17 @@ class Compilador{
                 case ')':
                     p--
                     if(p == 0){
-                        end = neg_f? k + 1 : k
+                        end = neg_f? ~~k + 1 : ~~k
                         neg_f = false
                         data.push(string.substr(start + 1, end - start - 1))
-                        start = k + 1
+                        start = ~~k + 1
                         last = true
                     }
                     break
                 case '¬':
-                    if(temp[k + 1] == '('){
+                    if(temp[~~k + 1] == '('){
                         if(neg_f == false){
-                            start = k - 1
+                            start = ~~k - 1
                             neg_f = true
                         }
                     }
@@ -145,7 +160,7 @@ class Compilador{
             }
         }
         if(last == false){
-            end = k
+            end = ~~k
             data.push(string.substr(start, end - start + 1))
         }
         return data.filter(a=>a)
@@ -155,25 +170,36 @@ class Compilador{
     }
     _printArrayP(array){
         for(var k in array){
-            if(Array.isArray(array[k])){
-                this._printArrayP(array[k])
+            if(Array.isArray(array[~~k])){
+                this._printArrayP(array[~~k])
             }else{
-                this._calcFunction(array[k])
+                this._calcFunction(array[~~k])
             }
         }
         return this
     }
-    _printVars(){}
+    _printVars(){
+        for(var k of this._getVars()){
+            this.result += `<th>${this._addSpacesBetweenChars(k)}</th>`
+            this.vars_array.push(this._addSpacesBetweenChars(k))
+        }
+        for(var k of this._getVarsNegative()){
+            this.result += `<th>${this._addSpacesBetweenChars(k)}</th>`
+            this.vars_array.push(this._addSpacesBetweenChars(k))
+        }
+        return this
+    }
     _getVars(){
-        for(var k in (temp = [...this.exp])){
-            if(temp[k].match(/^[a-zA-Z]+$/g)){
-                if(!this.vars.includes(temp[k])){
-                    this.vars.push(temp[k])
+        var temp = [...this.exp]
+        for(var k in temp){
+            if(temp[~~k].match(/^[a-zA-Z]+$/g)){
+                if(!this.vars.includes(temp[~~k])){
+                    this.vars.push(temp[~~k])
                 }
             }
         }
         var cant_v = this.vars.length
-        var cant_r = cant_v ** 2
+        var cant_r = 2 ** cant_v
         var exp_c = 2;
         for(var k of this.vars){
             var cant_vf = cant_r / exp_c
@@ -191,11 +217,12 @@ class Compilador{
         return this.vars
     }
     _getVarsNegative(){
-        for(var k in (temp = [...this.exp])){
-            if(temp[k].match(/^[a-zA-Z]+$/g)){
-                if(k > 0 && temp[k-1] == '¬'){
-                    if(!this.vars_negative.includes(`¬${temp[k]}`)){
-                        this.vars_negative.push(`¬${temp[k]}`)
+        var temp = [...this.exp]
+        for(var k in temp){
+            if(temp[~~k].match(/^[a-zA-Z]+$/g)){
+                if(~~k > 0 && temp[~~k-1] == '¬'){
+                    if(!this.vars_negative.includes(`¬${temp[~~k]}`)){
+                        this.vars_negative.push(`¬${temp[~~k]}`)
                     }
                 }
             }
@@ -204,5 +231,49 @@ class Compilador{
             this.vars_r[k] = this.vars_r[[...k][1]].reverse() // aqui va a dar algun problema
         }
         return this.vars_negative
+    }
+    _calcFunction(exp){
+        var data = this._customPregMatch(exp)
+        var cant_v = this.vars.length
+        var cant_r = 2 ** cant_v, arr2 = []
+        for(var i = 0; i < cant_r; i++){
+            var code = ""
+            for(var d of data){
+                switch (d) {
+                    case '∧':
+                        code += ' && '
+                        break;
+                    case '∨':
+                        code += ' || '
+                        break;
+                    case '→':
+                        code += ' <= '
+                        break;
+                    case '↔':
+                        code += ' == '
+                        break;
+                    case '⊕':
+                        code += ' ^ '
+                        break;
+                    default:
+                        code += `this.vars_r["${d}"][${i}]`
+                        break;
+                }
+            }
+            arr2.push(eval(code))
+        }
+        this.vars_r[exp] = arr2
+        this.result += `<th>${this._addSpacesBetweenChars(exp)}</th>`
+        this.vars_array.push(this._addSpacesBetweenChars(exp))
+        if(this._hasNegative(exp)){
+            this.vars_r[`¬(${exp})`] = arr2.map(a=>!a)
+            this.result += `<th>¬(${this._addSpacesBetweenChars(exp)})</th>`
+            this.vars_array.push(`¬(${this._addSpacesBetweenChars(exp)})`)
+        }
+        return this
+    }
+    _hasNegative(exp){
+        var f_exp = `¬(${exp})`
+        return this.exp.indexOf(f_exp) > -1
     }
 }
